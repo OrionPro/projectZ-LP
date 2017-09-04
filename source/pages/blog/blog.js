a714b89ffbb891fb55c8a50e8a1ba53c183666f7
OrionPro <orion79@rambler.ru>
2017-09-04 12:24:07 +0300

    first commit

---
+++ source/pages/blog/blog.js
@@ -0,0 +1,24 @@
+
+import common from '../../js/common';
+
+import './blog.sass';
+
+import animate from '../../js/animate_blog';
+
+console.log('in blog.js');
+
+$(document).ready( function() {
+	$("body").addClass("blog extinguisher-transition");
+});
+
+$(window).resize(function() {
+
+});
+
+$(window).scroll(function() {
+
+});
+
+
+$(".loader_inner").fadeOut();
+$(".loader").fadeOut("fast");
