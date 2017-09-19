6c659cc851affe10645af573e9a08bc5348c336c
OrionPro <orion79@rambler.ru>
2017-09-19 15:41:12 +0300

    15 50 19.09.2017

--- source/js/animate.js
+++ source/js/animate.js
@@ -8,11 +8,22 @@ class Animation {
 		this.logo = new TimelineMax();
 		this.logo = new TimelineMax();
 		this.tl2 = new TimelineMax();
+		this.tl3 = new TimelineMax();
+		this.tl4 = new TimelineMax();
+		this.tl5 = new TimelineMax();
+		this.tl6 = new TimelineMax();
+		this.tl7 = new TimelineMax();
 
 
 		this.tl1.pause();
 		this.logo.pause();
 		this.tl2.pause();
+		this.tl3.pause();
+		this.tl4.pause();
+		this.tl5.pause();
+		this.tl6.pause();
+		this.tl6.pause();
+
 	}
 
 	description() {
@@ -63,7 +74,61 @@ class Animation {
 				opacity: 0,
 				ease: Power1.easeOut
 			}, 0.4, '-=0.2');
-
+		this.tl3.from('.how-it-works .how-it-works__title h2', 0.5, {
+			y: -50,
+			opacity: 0,
+			ease: Power4.easeOut
+		}, 0.5, '+=0.5')
+			.staggerFrom('.how-it-works__item', 0.8, {
+				y: -50,
+				opacity: 0,
+				ease: Power4.easeOut
+			}, 0.3, '-=0.2')
+			.staggerFrom('.how-it-works__item img', 0.8, {
+				y: -50,
+				opacity: 0,
+				ease: Power4.easeOut
+			}, 0.3, '-=1.8');
+		this.tl4.from('.material-requirements .material-requirements__title h2', 0.5, {
+			y: -50,
+			opacity: 0,
+			ease: Power4.easeOut
+		}, 0.5, '+=0.5')
+			.staggerFrom('.material-requirements__item', 0.8, {
+				y: -50,
+				opacity: 0,
+				ease: Power4.easeOut
+			}, 0.3, '-=0.2')
+			.staggerFrom('.material-requirements__item img', 0.8, {
+				y: -50,
+				opacity: 0,
+				ease: Power4.easeOut
+			}, 0.3, '-=1.8');
+		this.tl5.from('.post-your-publication .post-your-publication__title h2', 0.5, {
+			y: -50,
+			opacity: 0,
+			ease: Power4.easeOut
+		}, 0.5, '+=0.5')
+			.from('.post-your-publication__carousel', 0.5, {
+			scaleX: 0,
+			opacity: 0,
+			ease: Power4.easeOut
+		}, 0.5, '+=0.3');
+		this.tl6.from('.get-more .get-more__title h2', 0.5, {
+			y: -50,
+			opacity: 0,
+			ease: Power4.easeOut
+		}, 0.5, '+=0.5')
+			.staggerFrom('.get-more__title p', 0.8, {
+				y: -50,
+				opacity: 0,
+				ease: Power4.easeOut
+			}, 0.3, '-=0.2')
+			.staggerFrom('.get-more .get-more__title .anim', 0.5, {
+				y: -50,
+				opacity: 0,
+				ease: Power4.easeOut
+			}, 0.5);
 	}
 
 	activeSection(section, startTop = 0, startBotton = 0) {
@@ -85,6 +150,22 @@ class Animation {
 		}
 		if (this.activeSection('our-advantages', 500, 200)) {
 			this.tl2.resume();
+
+		}
+		if (this.activeSection('how-it-works', 500, 200)) {
+			this.tl3.resume();
+
+		}
+		if (this.activeSection('material-requirements', 500, 200)) {
+			this.tl4.resume();
+
+		}
+		if (this.activeSection('post-your-publication', 500, 200)) {
+			this.tl5.resume();
+
+		}
+		if (this.activeSection('get-more', 800, 0)) {
+			this.tl6.resume();
 
 		}
 	}
